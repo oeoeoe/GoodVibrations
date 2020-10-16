9fc76fd85c121dfabb283001ce2a475fc00fb528
AllexNordgren <allexnordgren@outlook.com>
2020-10-16 13:45:46 +0200

    taps are showing

--- app/src/main/java/com/example/goodvibrationsapp/MainActivity.kt
+++ app/src/main/java/com/example/goodvibrationsapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.goodvibrationsapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,11 +13,11 @@ import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -23,4 +26,6 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+
 }
