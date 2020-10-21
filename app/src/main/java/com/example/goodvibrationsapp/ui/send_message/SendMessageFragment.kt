42c08d4c581b84a9a827aa741715d60e1bae86c2
AllexNordgren <allexnordgren@outlook.com>
2020-10-21 11:32:07 +0200

    Update SendMessageFragment.kt

--- app/src/main/java/com/example/goodvibrationsapp/ui/send_message/SendMessageFragment.kt
+++ app/src/main/java/com/example/goodvibrationsapp/ui/send_message/SendMessageFragment.kt
@@ -1,22 +1,14 @@
 package com.example.goodvibrationsapp.ui.send_message
 
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.goodvibrationsapp.MainActivity
-import com.example.goodvibrationsapp.Phrase
 import com.example.goodvibrationsapp.R
 import kotlinx.android.synthetic.main.fragment_send_message.*
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.fragment_dictionary.*
 
 
 //previously known as HomeFragment
@@ -30,7 +22,7 @@ class SendMessageFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_send_message, container, false)
         val sendButton :Button = root.findViewById(R.id.button_send_message)
         sendButton.setOnClickListener{
-            vibratePhone(taps_view.getCodeSequence())
+            sendMessage(taps_view.getCodeSequence())
         }
         return root
     }
@@ -40,13 +32,12 @@ class SendMessageFragment : Fragment() {
 
     }
 
-    fun vibratePhone(message :String) {
+    fun sendMessage(message :String) {
         // Write a message to the database
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
 
         myRef.setValue(message)
-        (activity as MainActivity?)?.vibratePhone(message)
         taps_view.resetTaps()
     }
 }
